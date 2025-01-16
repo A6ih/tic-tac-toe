6a2c42040075ffabf52730c13f8b75baa4aed43d
A6ih <177154114+A6ih@users.noreply.github.com>
2025-01-16 23:13:04 +0200

    Add activePlayer and switchActiveplayer function and Add function to check winner for rows and column

--- index.js
+++ index.js
@@ -10,45 +10,81 @@ function gameBoard() {
             }
         }
     }
+    const boardColumn = [];
+    function getColumns() {
+        for (let i = 0; i < size; i++) {
+            boardColumn[i] = [];
+            for (let j = 0; j < size; j++) {
+                boardColumn[i].push(board[j][i]);
+            }
+        }       
+    }
     const getBoard = () => board;
     const insertMarker = function(marker, row, column) {
         if(board[row][column] === " ") {
             board[row][column] = marker;
         }
     }
-    return {createNewBoard, getBoard, insertMarker};
+    const checkWinner = function(marker) {
+        let getWinner = false;
+        const checkRows = board.map(item => item.every(value => value === marker));
+        getColumns()
+        const checkColumns = boardColumn.map(item => item.every(value => value === marker));
+        if(checkRows.includes(true) ||
+           checkColumns.includes(true)) {
+            getWinner = true;
+        }
+        return getWinner;
+    }
+    return {createNewBoard, getBoard, insertMarker, checkWinner};
 }
 
 function createPlayer(name, marker) {
-    let input = 0;
+    let moves = 0;
     const getName = () => name;
     const getMarker = () => marker;
-    const addInput = () => input++;
-    const getInput = () => input;
-    return {getName, getMarker, addInput, getInput}
+    const addMoves = () => moves++;
+    const getMoves = () => moves;
+    return {getName, getMarker, addMoves, getMoves}
 }
 
 const playerOne = createPlayer("Player 1","X");
 const playerTwo = createPlayer("Player 2", "O");
+const players = [playerOne, playerTwo]
+let activePlayer = players[0];
+
+function switchActivePlayer() {
+    activePlayer === players[0] ? activePlayer = players[1] : activePlayer = players[0];
+}
 
 function startGame() {
     const board = gameBoard();
     board.createNewBoard();
     function playRound(marker, row, column) {
         board.insertMarker(marker, row, column);
-        console.log(board.getBoard());
+        // console.log(board.getBoard());
     }
-    return {playRound};
+    return {playRound, board};
 }
 
-// const Game1 = startGame();
-// Game1.playRound(playerOne.getMarker(),0,1);
-// playerOne.addInput();
-// Game1.playRound(playerTwo.getMarker(),0,1);
-// Game1.playRound(playerOne.getMarker(),1,1);
-// playerOne.addInput();
-// Game1.playRound(playerTwo.getMarker(),2,1);
-// console.log(playerOne.getInput());
+const Game1 = startGame();
+Game1.playRound(playerOne.getMarker(),0,1);
+playerOne.addMoves();
+Game1.playRound(playerTwo.getMarker(),0,1);
+Game1.playRound(playerOne.getMarker(),1,1);
+playerOne.addMoves();
+Game1.playRound(playerTwo.getMarker(),2,1);
+Game1.playRound(playerOne.getMarker(),0,2);
+console.log(playerOne.getMoves());
+// console.log(activePlayer.getMarker())
+console.log(Game1.board.checkWinner(playerOne.getMarker()));
+Game1.playRound(playerOne.getMarker(),1,2);
+Game1.playRound(playerOne.getMarker(),2,2);
+console.log(Game1.board.getBoard());
+console.log(Game1.board.checkWinner(playerOne.getMarker()));
+console.log(playerOne.getMoves());
+
+
 
 
 
