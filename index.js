0b8bd5f22152229937abc8befd52e267c179053f
A6ih <177154114+A6ih@users.noreply.github.com>
2025-01-17 00:24:55 +0200

    Add functions to check diagonal win condition, playable on console

--- index.js
+++ index.js
@@ -19,20 +19,56 @@ function gameBoard() {
             }
         }       
     }
+    const rightDiagonal = []
+    function getRightDiagonal() {
+        for (let i = 0; i < size; i++) {
+            rightDiagonal[i] = [];
+            for (let j = 0; j < size; j++) {
+                rightDiagonal[i].push(board[j][j]);
+            }
+        }     
+    }
+    const leftDiagonal = []
+    function getLeftDiagonal() {
+        const leftSize = size - 1;
+        for (let i = 0; i < size; i++) {
+            leftDiagonal[i] = [];
+            for (let j = 0; j < size; j++) {
+                leftDiagonal[i].push(board[j][leftSize - j]);
+            }
+        }     
+    }
     const getBoard = () => board;
     const insertMarker = function(marker, row, column) {
         if(board[row][column] === " ") {
             board[row][column] = marker;
         }
+        else {
+            switchActivePlayer();
+            console.log("Marker already exist! find a empty spot!")
+        }
     }
     const checkWinner = function(marker) {
         let getWinner = false;
-        const checkRows = board.map(item => item.every(value => value === marker));
+        const checkRows = board.
+                          map(item => item.every(value => value === marker));
         getColumns()
-        const checkColumns = boardColumn.map(item => item.every(value => value === marker));
+        const checkColumns = boardColumn.
+                             map(item => item.every(value => value === marker));
+        getRightDiagonal()
+        const checkRightDiagonal = rightDiagonal.
+                                   map(item => item.every(value => value === marker));
+        getLeftDiagonal()
+        const checkLeftDiagonal = leftDiagonal.
+                                  map(item => item.every(value => value === marker));
         if(checkRows.includes(true) ||
-           checkColumns.includes(true)) {
+           checkColumns.includes(true) ||
+           checkRightDiagonal.includes(true) ||
+           checkLeftDiagonal.includes(true)) {
             getWinner = true;
+        }
+        else if(!board.flat().includes(" ")) {
+            getWinner = "Draw";
         }
         return getWinner;
     }
@@ -60,29 +96,37 @@ function switchActivePlayer() {
 function startGame() {
     const board = gameBoard();
     board.createNewBoard();
-    function playRound(marker, row, column) {
-        board.insertMarker(marker, row, column);
+    function playRound(row, column) {
+        console.log(`Current turn : Player: ${activePlayer.getName()}, Marker: ${activePlayer.getMarker()}`);
+        board.insertMarker(activePlayer.getMarker(), row, column);
+        console.log(board.getBoard());
+        if (board.checkWinner(activePlayer.getMarker()) === true) {
+            console.log(activePlayer.getName() + " wins!");
+        }
+        else if (board.checkWinner(activePlayer.getMarker()) === "Draw") {
+            console.log("It's a Draw!");
+        }
+        switchActivePlayer();
+        console.log(`Next Turn : Player: ${activePlayer.getName()}, Marker: ${activePlayer.getMarker()}`);
         // console.log(board.getBoard());
     }
     return {playRound, board};
 }
 
 const Game1 = startGame();
-Game1.playRound(playerOne.getMarker(),0,1);
-playerOne.addMoves();
-Game1.playRound(playerTwo.getMarker(),0,1);
-Game1.playRound(playerOne.getMarker(),1,1);
-playerOne.addMoves();
-Game1.playRound(playerTwo.getMarker(),2,1);
-Game1.playRound(playerOne.getMarker(),0,2);
-console.log(playerOne.getMoves());
-// console.log(activePlayer.getMarker())
-console.log(Game1.board.checkWinner(playerOne.getMarker()));
-Game1.playRound(playerOne.getMarker(),1,2);
-Game1.playRound(playerOne.getMarker(),2,2);
-console.log(Game1.board.getBoard());
-console.log(Game1.board.checkWinner(playerOne.getMarker()));
-console.log(playerOne.getMoves());
+Game1.playRound(0,0);
+Game1.playRound(0,1);
+Game1.playRound(1,0);
+Game1.playRound(2,0);
+Game1.playRound(0,2);
+Game1.playRound(1,1);
+Game1.playRound(2,2);
+Game1.playRound(2,1);
+
+
+
+
+
 
 
 
